ff5da6bba39b3c4bf20789d72cb5985d9b9e6d28
LRR <673034505@qq.com>
2020-08-21 17:23:38 +0800

    项目初始化
    项目初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,29 @@
+
+<template>
+         <view class="container">
+		
+          </view>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+				
+            }
+        },
+		onLoad() {
+			setTimeout(function(){
+				//关闭启动界面。app端才会执行 其他不执行
+                // #ifdef  APP-PLUS  
+                    plus.navigator.closeSplashscreen() 
+                // #endif
+			},3000)
+		},
+        methods: {
+			
+        },
+    }
+</script>
+<style>
+    
+</style>
---
+++ pages/web_view/web_view.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+		<!-- web-view 使用列子 -->
+		<web-view  :src="url"  :webview-styles="webviewStyles" v-if="url.length>0" ></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url:'',
+				webviewStyles: {
+					progress: false,
+				},
+				id:''
+			};
+		},
+		onLoad(options) {
+			if (options && options.id) {//如果需要传值，用值拼接
+				this.id = options.id
+			}
+			 this.id = 100
+			 let _self = this;
+			uni.getSystemInfo({
+			    success: function (res) {
+					//动态设置屏幕高度和宽度
+					_self.url = `/hybrid/html/user.html?pageW=${res.windowWidth}&id=${_self.id}`
+			    }
+			});
+		},
+	}
+</script>
+
+<style lang="less">
+
+</style>
